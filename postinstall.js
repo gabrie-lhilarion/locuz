dcdc1c68982a6036b52a848b712bfcc4bffecfa1
Vasanth.V <imvasanthv@gmail.com>
2022-01-26 21:37:37 +0530

    Added console to postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 if (process.env.NODE_ENV) {
+	console.log("Setting up service worker version...");
 	const serviceWorkerContents = fs.readFileSync(path.join(__dirname, "www/sw.js")).toString();
 	const VERSION = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"))).version;
 
